11e71911bc33eaab59fa2c8f8e4dcc739528b79a
RDx <rodrigo.apaza.m@tecsup.edu.pe>
2024-11-17 02:08:46 -0500

    chore: Añadir dependencias de navegación y ViewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,4 +85,8 @@ dependencies {
     implementation ("androidx.core:core-ktx:1.10.1")
 
     implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.5")
+
+    // Dependencies for navigation and lifecycle
+    implementation("androidx.navigation:navigation-compose:2.8.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
 }
